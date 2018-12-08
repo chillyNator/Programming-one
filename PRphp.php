e9fb7d5ac8fad9a76cff502226809e339436a418
chillyNatorgit config --global user.name chillyNator <don.chilly2011@yandex.ru>
2018-12-08 17:39:12 +0300

    first commit

--- PRphp.php
+++ PRphp.php
@@ -1,27 +1,15 @@
 <?php
+if ($operationType === '+') {
+    echo plus($chislo1, $chislo2);
+} elseif ($operationType === '-') {
+    echo minus($chislo1, $chislo2);
+} elseif ($operationType === '*') {
+    echo umnojenie($chislo1, $chislo2);
+}elseif ($operationType === '/')
+    echo delenie($chislo1, $chislo2);
+    $chislo1="5";
+    $chislo2="2";
+    function kalk 
 
-$money_zp = 25000;
-$kredit = 15700;
-$heals = 3000;
-$last = $money_zp || $kredit || $heals;
-echo  $last;
 
-$c = 1;
-echo 'Privet ti v igre ygadai shislo!' . PHP_EOL;
 
-$secretNumber = rand(0, 100);
-
-while (true) {
-    echo 'Vvedite shislo:' . PHP_EOL;
-
-    $printedNumber = (int) fgets(STDIN);
-
-    if ($printedNumber > $secretNumber) {
-        echo 'Vi vveli: ' . $printedNumber . ', eto sliwkom mnogo!' . PHP_EOL;
-    } elseif ($printedNumber < $secretNumber) {
-        echo 'Vi vveli: ' . $printedNumber . ', eto sliwkom malo!' . PHP_EOL;
-    } else {
-        echo 'Pozdravlaem vi win!';
-        break;
-    }
-}
