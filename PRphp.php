a4e900ae39c3db7f7b62552fe2ae3a2084b0d78e
chillyNatorgit config --global user.name chillyNator <don.chilly2011@yandex.ru>
2018-12-10 17:30:39 +0300

    first commit

--- PRphp.php
+++ PRphp.php
@@ -1,15 +1,42 @@
 <?php
-if ($operationType === '+') {
-    echo plus($chislo1, $chislo2);
-} elseif ($operationType === '-') {
-    echo minus($chislo1, $chislo2);
-} elseif ($operationType === '*') {
-    echo umnojenie($chislo1, $chislo2);
-}elseif ($operationType === '/')
-    echo delenie($chislo1, $chislo2);
-    $chislo1="5";
-    $chislo2="2";
-    function kalk 
+echo 'Hello this is a calculator' . PHP_EOL;
+{
+    echo 'Insert the number' . PHP_EOL;
+
+    $operationType = (int)fgets(STDIN);
+
+
+    function plus($chislo1, $chislo2)
+    {
+        return $chislo1 + $chislo2;
+    }
+
+    function minus($chislo1, $chislo2)
+    {
+        return $chislo1 - $chislo2;
+    }
+
+    function multiplication($chislo1, $chislo2)
+    {
+        return $chislo1 * $chislo2;
+    }
+
+    function division($chislo1, $chislo2)
+    {
+        return $chislo1 / $chislo2;
+    }
+
+    if ($operationType === '+') {
+        echo plus($chislo1, $chislo2);
+    } elseif ($operationType === '-') {
+        echo minus($chislo1, $chislo2);
+    } elseif ($operationType === '*') {
+        echo multiplication($chislo1, $chislo2);
+    } elseif ($operationType === '/') {
+        echo division($chislo1, $chislo2);
+    }
+}
+
 
 
 
